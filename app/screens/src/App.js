987238b0fb43a27f267c83fecb84d45bd1d22195
mydrobeofficial <anamayataylor@gmail.com>
2026-03-11 03:17:04 +1300

    Create App.js to display MydrobeMVP3
    Main App component that imports and displays the MydrobeMVP3 component for the MVP interface.

---
+++ app/screens/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MydrobeMVP3 from './MydrobeMVP3';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to MyDrobe MVP v3</h1>
+      <MydrobeMVP3 />
+    </div>
+  );
+}
