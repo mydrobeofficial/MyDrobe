b5b96f9b7e7777d2f64f52bf4135ae1840d81818
mydrobeofficial <anamayataylor@gmail.com>
2026-03-11 03:33:23 +1300

    Backup: original React web version
    Backup of MydrobeMVP3 web React component before converting to React Native for Expo Go.

---
+++ app/screens/src/MydrobeMVP3_backup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function MydrobeMVP3() {
+  return (
+    <div>
+      <h2>MyDrobe MVP v3</h2>
+      <p>This is the main interface for the MyDrobe app.</p>
+    </div>
+  );
+}
