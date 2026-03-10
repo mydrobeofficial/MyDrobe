b43f4cd369159d95e4a647e736ce93bf02153a27
mydrobeofficial <anamayataylor@gmail.com>
2026-03-11 02:48:35 +1300

    Create WardrobeScreen component
    This file contains the main React component for the MyDrobe MVP v3 interface.

---
+++ app/screens/WardrobeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function WardrobeScreen() {
+  return (
+    <View>
+      <Text>Wardrobe Screen</Text>
+    </View>
+  );
+}
